de91adfabe8e957eaf82d7449957bb19d4f9a501
Purdea Andrei <andrei@purdea.ro>
2023-01-31 13:28:32 +0200

    PR SENT: fix keys that are not found in transform

--- app/src/matrix_transform.c
+++ app/src/matrix_transform.c
@@ -13,14 +13,15 @@
 
 #define _TRANSFORM_ENTRY(i, _)                                                                     \
     [(KT_ROW(DT_PROP_BY_IDX(ZMK_KEYMAP_TRANSFORM_NODE, map, i)) * ZMK_MATRIX_COLS) +               \
-        KT_COL(DT_PROP_BY_IDX(ZMK_KEYMAP_TRANSFORM_NODE, map, i))] = i
+        KT_COL(DT_PROP_BY_IDX(ZMK_KEYMAP_TRANSFORM_NODE, map, i))] = i + 1
 
 static uint32_t transform[] = {LISTIFY(ZMK_KEYMAP_LEN, _TRANSFORM_ENTRY, (, ), 0)};
 
 #endif
 
-uint32_t zmk_matrix_transform_row_column_to_position(uint32_t row, uint32_t column) {
+int32_t zmk_matrix_transform_row_column_to_position(uint32_t row, uint32_t column) {
     uint32_t matrix_index;
+    uint32_t value;
 
 #if DT_NODE_HAS_PROP(ZMK_KEYMAP_TRANSFORM_NODE, col_offset)
     column += DT_PROP(ZMK_KEYMAP_TRANSFORM_NODE, col_offset);
@@ -33,7 +34,12 @@ uint32_t zmk_matrix_transform_row_column_to_position(uint32_t row, uint32_t colu
     matrix_index = (row * ZMK_MATRIX_COLS) + column;
 
 #ifdef ZMK_KEYMAP_TRANSFORM_NODE
-    return transform[matrix_index];
+    if (matrix_index >= ARRAY_SIZE(transform))
+        return -1;
+    value = transform[matrix_index];
+    if (!value)
+        return -1;
+    return value - 1;
 #else
     return matrix_index;
 #endif /* ZMK_KEYMAP_TRANSFORM_NODE */

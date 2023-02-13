3d55f872c2a0787d3ed7d8809ea62c6c0f7b56b1
Purdea Andrei <andrei@purdea.ro>
2023-02-13 06:35:30 +0200

    Add support for disabling the numlock locklight affecting layers behavior

--- app/boards/arm/minirazz/init.c
+++ app/boards/arm/minirazz/init.c
@@ -24,18 +24,20 @@ static int local_gpio_init(const struct device *port) {
     return 0;
 }
 
+#ifndef CONFIG_DISABLE_NUMLOCK_TOGGLES_LAYER
 static int numlock_listener(const zmk_event_t *eh) {
     const struct zmk_led_changed *ev = as_zmk_led_changed(eh);
     zmk_leds_flags_t leds = ev->leds;
     if (leds & ZMK_LED_NUMLOCK_BIT) {
-        zmk_keymap_layer_activate(1);
+        zmk_keymap_layer_activate(CONFIG_NUMLOCK_TOGGLES_THIS_LAYER);
     } else {
-        zmk_keymap_layer_deactivate(1);
+        zmk_keymap_layer_deactivate(CONFIG_NUMLOCK_TOGGLES_THIS_LAYER);
     }
     return ZMK_EV_EVENT_BUBBLE;
 }
 
 ZMK_LISTENER(numlock_listener, numlock_listener);
 ZMK_SUBSCRIPTION(numlock_listener, zmk_led_changed);
+#endif
 
 SYS_INIT(local_gpio_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
